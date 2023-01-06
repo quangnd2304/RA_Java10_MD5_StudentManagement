36f8951a27d0556935de19b40eb137dfcc1326b4
QuangND <quangnd.dev@gmail.com>
2023-01-06 16:26:33 +0700

    Student Management

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import Control from './components/Control';
+import ListStudent from './components/ListStudent';
+import Form from './components/Form';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="row">
+          <div className="col-lg-7 grid-margin stretch-card">
+            <div className="card">
+              {/* START CONTROL */}
+              <Control></Control>
+              {/* END CONTROL */}
+              {/* START LIST STUDENT */}
+              <ListStudent></ListStudent>
+              {/* END LIST STUDENT */}
+            </div>
+          </div>
+          {/* START FORM SINH VIEN */}
+          <Form></Form>
+          {/* END FORM SINH VIÊN */}
+        </div>
+
+      </div>
+    );
+  }
 }
 
 export default App;
